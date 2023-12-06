6d84dc021c83e89853f94e9dc1c49225489ec1ca
MuhammadRaihanAlghifari <144991179+MuhammadRaihanAlghifari@users.noreply.github.com>
2023-12-06 23:52:13 +0700

    Add files via upload

---
+++ PBO12/src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        dataPembelian Beli = new faktur(); // polymorphisme
+
+        Beli.isidata();
+        Beli.cetakFaktur();
+
+    }
+}
---
+++ PBO12/src/dataPembelian.java
@@ -0,0 +1,22 @@
+
+public class dataPembelian implements cetak { // implement interface cetak
+    public String NamaToko = "Toko store";
+    public String StrukPelanggan = "data pelanggan";
+    public String StrukPelanggan2 = "data pembelian barang";
+    public String nofaktur;
+    public String namaPelanggan;
+    public String alamat;
+    public String noHP;
+    public String kodeBarang;
+    public String namaBarang;
+    public Integer hargaBarang;
+    public Integer jumlahBeli;
+    public Integer totalBayar;
+
+    public void isidata() {
+    }
+
+    @Override
+    public void cetakFaktur() {
+    }
+}
